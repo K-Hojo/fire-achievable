103bff49fc0cbafdcb667e6bdbe76876f486fada
K-Hojo <k.hojo1392@gmail.com>
2023-06-30 23:30:10 +0000

    initial commit

---
+++ dockerfile
@@ -0,0 +1,3 @@
+FROM node:18-bullseye-slim
+WORKDIR /src
+RUN npm install -g npm@latest
---
+++ src/prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  jsxSingleQuote: true,
+}
